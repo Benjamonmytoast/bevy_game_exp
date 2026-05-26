4348b2dcff6770310c92e07d0f036cfd2801edbc
Benjamin Stephenson <1999bstephenson@gmail.com>
2026-05-26 22:18:55 +0100

    Simple Hello World App.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
+fn hello_world() {
+    println!("Hello World!")
+}
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, hello_world)
+        .run();
 }
